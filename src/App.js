4d65d902e7ec79fe6f3028403868cf03100de768
Ethan Shealey <ethan.shealey@gmail.com>
2022-02-01 21:03:00 -0500

    adding to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Row from './components/Row'
+import words from './words/word-list.json'
+import answers from './words/answer-list.json'
 
 function App() {
+  
+  const [input, setInput] = useState('')
+  const [row, setRow] = useState(0)
+  const [currentBlock, setCurrentBlock] = useState('00')
+  const [isLoading, setIsLoading] = useState(true)
+
+  const [word, setWord] = useState('')
+  const [allWords, setAllWords] = useState([])
+
+  useEffect(() => {
+    setIsLoading(true)
+    setAllWords(words.words)
+    setWord(answers.words[Math.floor(Math.random() * answers.words.length)].toUpperCase())
+    setIsLoading(false)
+  }, [])
+
+  const stopClick = () => {
+    setTimeout(() => document.getElementById(currentBlock).focus(), 20)
+  }
+
+  const handleAnswer = (ans) => {
+    ans.forEach((ele, ind) => {
+      if(word[ind].toUpperCase() === ele) {
+        const block = document.getElementById(`${row}${ind}`)
+        block.style.backgroundColor = '#538d4e'
+        block.style.borderColor = '#538d4e'
+      }
+      else if(word.toUpperCase().includes(ele)) {
+        const block = document.getElementById(`${row}${ind}`)
+        block.style.backgroundColor = '#b59f3b'
+        block.style.borderColor = '#b59f3b'
+      }
+      else {
+        const block = document.getElementById(`${row}${ind}`)
+        block.style.backgroundColor = '#3a3a3c'
+        block.style.borderColor = '#3a3a3c'
+      }
+    })
+    if(ans.join('') === word.toUpperCase()) {
+      alert('you won!')
+      const inputs = [].slice.call(document.getElementsByTagName('input')).forEach(ele => ele.disabled = true)
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="main" onClick={() => stopClick()}></div>
+        { !isLoading ? (
+        <div className='wordle-wrapper'>
+          <h1 onClick={stopClick}>W O R D L E</h1>
+          <hr />
+          <br />
+          {
+            Array(6).fill(0).map((_, i) => {
+              return (
+                <Row
+                  key={i}
+                  row={row}
+                  index={i}
+                  input={input}
+                  setInput={setInput}
+                  setRow={setRow}
+                  setCurrentBlock={setCurrentBlock}
+                  handleAnswer={handleAnswer}
+                />
+              )})
+            }
+        </div>
+        ) : (
+          <div className='wordle-wrapper'><h1>Loading...</h1></div>
+        )}
+      </>
   );
 }
 
---
+++ src/components/Row/index.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+
+const Row = (props) => {
+
+    const [isDisabled, setIsDisabled] = useState(false)
+    const [values, setValues] = useState([
+        '', '', '' , '', ''
+    ])
+
+    useEffect(() => {
+        if(!values.includes('')) {
+            document.getElementById(`${props.index+1}0`).focus()  
+            props.setCurrentBlock(`${props.index+1}0`)
+        }      
+    }, [props.row])
+
+    const handleInput = (e, index) => {
+        if(e.key === "Backspace") { 
+            setValues(v => v.map((ele, ind) => { return ind === index ? '' : ele }))
+            if(index !== 0 && values[index] === '') {
+                document.getElementById(`${props.index}${index-1}`).focus()
+                setValues(v => v.map((ele, ind) => { return ind === index-1 ? '' : ele }))
+                props.setCurrentBlock(`${props.index}${index-1}`)
+            }
+        }
+        else if(e.key === "Enter") {
+            if(!values.includes('')) {
+                if(props.index !== 5) props.setRow(r => r+1)
+                setIsDisabled(true)
+                props.handleAnswer(values)
+            }
+        }
+        else if(e.keyCode >= 65 && e.keyCode <= 90 && values[index] === '') {
+            setValues(v => v.map((ele, ind) => { return ind === index ? e.key.toUpperCase() : ele }))
+            index !== 4 && document.getElementById(`${props.index}${index+1}`).focus()
+            if(index !== 4) props.setCurrentBlock(`${props.index}${index+1}`)
+        }
+    }
+
+    return (
+        <>
+            {
+                Array(5).fill(1).map((_, i) => {
+                    return (
+                        <input id={props.index + '' + i} value={values[i]} className={`input-block ${values[i] === '' ? '' : 'bordered'}`} disabled={props.row !== props.index || isDisabled} maxLength={1} autoFocus={props.index + '' + i === `${props.index}0`} onMouseDown={(e) => e.preventDefault()} onKeyDown={(e) => handleInput(e, i)}/>
+                    )
+                })
+            }
+        </>
+    );
+};
+
+export default Row;
